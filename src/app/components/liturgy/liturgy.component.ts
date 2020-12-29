cf0b0d5509b6c4f4e92f66bfef778d6762cb7891
Dmitry Svirbutovich <bixboxdima@gmail.com>
2020-12-29 12:11:50 +0300

    fixed bug with liturgy

--- src/app/components/liturgy/liturgy.component.ts
+++ src/app/components/liturgy/liturgy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Liturgy } from '../../interfaces/liturgy';
 import { LiturgyService } from '../../services/liturgy-service/liturgy.service';
 import { Observable } from 'rxjs';
@@ -7,9 +7,11 @@ import { Observable } from 'rxjs';
   selector: 'app-liturgy',
   templateUrl: './liturgy.component.html',
   styleUrls: ['./liturgy.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LiturgyComponent implements OnInit {
   liturgy$: Observable<Liturgy>;
+
   constructor(private liturgyService: LiturgyService) {}
 
   ngOnInit(): void {
